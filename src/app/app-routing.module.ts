490ff9480d4bc7b60850ff9a7a26838df6fd3606
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-03-27 12:16:19 +0200

    summary headline finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
+import { BoardComponent } from './pages/board/board.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ImpressComponent } from './pages/impress/impress.component';
+import { SummaryComponent } from './pages/summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'summary', component: SummaryComponent},
+  {path: 'board', component: BoardComponent},
+  {path: 'add-task', component: AddTaskComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'impress', component: ImpressComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { BoardComponent } from './pages/board/board.component';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ImpressComponent } from './pages/impress/impress.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent
+    SidenavComponent,
+    SummaryComponent,
+    BoardComponent,
+    AddTaskComponent,
+    ContactsComponent,
+    ImpressComponent
   ],
   imports: [
     AppRoutingModule,
