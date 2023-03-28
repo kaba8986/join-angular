1cdfaf65bfe5ec2d08b43d282f5a260f000f974e
Andreas Burghardt <andreas-burghardt@gmx.net>
2023-03-28 13:19:55 +0200

    not mutch done :)

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
-const materialComponents: any = [MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatSidenavModule];
+const materialComponents: any = [MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatSidenavModule];
 
 
 @NgModule({
