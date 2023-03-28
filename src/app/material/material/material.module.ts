a6776cb34ebde26b9bd6e326f944d8355f228087
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-03-28 12:15:10 +0200

    add task form left side

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
-const materialComponents: any = [MatFormFieldModule, MatIconModule, MatInputModule, MatSidenavModule];
+const materialComponents: any = [MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatSidenavModule];
 
 
 @NgModule({
