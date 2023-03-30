d70237f943650903a2a454b484fdeac9931fc8b0
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-03-30 15:03:26 +0200

    adding contacts working

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Contact } from 'src/app/models/contact.class';
 
@@ -14,14 +15,16 @@ export class AddContactComponent implements OnInit {
   newContact: Contact = new Contact();
 
   constructor(
-    public _dialogRef: MatDialogRef<AddContactComponent>
+    public _dialogRef: MatDialogRef<AddContactComponent>,
+    private _firestore: AngularFirestore
   ) { }
 
   ngOnInit(): void {
   }
 
   async createContact() {
-
+    this._firestore.collection('contacts').add(this.newContact.toJSON());   
+    this._dialogRef.close();
   }
 
   close(): void {
