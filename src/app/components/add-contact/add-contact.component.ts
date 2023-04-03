496ac5b4cc3173d3a5954bfb64ebdebf00e4cc77
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-04-03 10:46:59 +0200

    form validation for add contact

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Contact } from 'src/app/models/contact.class';
 
@@ -11,24 +12,44 @@ import { Contact } from 'src/app/models/contact.class';
 })
 export class AddContactComponent implements OnInit {
 
-
-  newContact: Contact = new Contact();
+  isLoading = false;
+  form: FormGroup;
 
   constructor(
     public _dialogRef: MatDialogRef<AddContactComponent>,
-    private _firestore: AngularFirestore
-  ) { }
-
-  ngOnInit(): void {
+    private _firestore: AngularFirestore,
+    private formBuilder: FormBuilder
+  ) { 
+    this.form = this.formBuilder.group({
+      firstName: [''],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['']
+    });
   }
 
-  async createContact() {
-    this._firestore.collection('contacts').add(this.newContact.toJSON());   
+  ngOnInit(): void {}
+
+ onSubmit() {
+    if(this.form.invalid) {
+      return;
+    } else {
+      this.createContact(this.form.value);
+    }
+  }
+
+  createContact(contact: Contact) {
+    this.isLoading = true;
+    let newContact = new Contact(contact);
+    this._firestore.collection('contacts').add(newContact.toJSON());   
     this._dialogRef.close();
+    this.isLoading = false;
   }
 
   close(): void {
     this._dialogRef.close();
   }
+
+
 
 }
