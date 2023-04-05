f3ee25372655dd5be1887e45762f3fb598a5053f
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-04-05 11:26:49 +0200

    contact page responsive

---
+++ src/app/pages/accordeon/accordeon.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordeon',
+  templateUrl: './accordeon.component.html',
+  styleUrls: ['./accordeon.component.scss']
+})
+export class AccordeonComponent implements OnInit {
+
+  panelOpenState: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
